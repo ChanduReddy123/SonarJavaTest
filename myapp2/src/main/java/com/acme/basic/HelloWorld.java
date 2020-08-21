30b4ff9debf3587d159589bb9c8145fb01b5998f
Chandrashekar <fk9w@eurofins.com>
2020-08-21 14:55:45 +0530

    updated

---
+++ myapp2/src/main/java/com/acme/basic/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.acme.basic;
+
+public class HelloWorld {
+
+  void sayHello() {
+    System.out.println("Hello World!");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests");
+  }
+  void notCovered1() {
+    System.out.println("This method is not covered by unit tests");
+  }
+  void notCovered2() {
+    System.out.println("This method is not covered by unit tests");
+  }
+  void notCovered3() {
+    System.out.println("This method is not covered by unit tests");
+  }
+
+}
