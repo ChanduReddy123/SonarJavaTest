5351c40b211d5ce34b715ab6b20183a4791fee1b
Chandrashekar <fk9w@eurofins.com>
2020-08-24 23:49:58 +0530

    push

---
+++ pipelineEnvVars.groovy
@@ -0,0 +1,16 @@
+def qa(){
+env.name = "chandu"
+env.age = "23"
+env.firetv = "NOK"
+env.flagship = "NOK"
+env.modules = "NOK"
+env.sharedui = "NOK"
+}
+def uat(){
+env.name = "reddy"
+env.age = "233"
+env.firetv = "NOK"
+env.flagship = "NOK"
+env.modules = "NOK"
+env.sharedui = "NOK"
+}
