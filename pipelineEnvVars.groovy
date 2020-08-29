de2df72f155c85b981d944844e9b9ee89cfdf12b
Chandrashekar <fk9w@eurofins.com>
2020-08-29 21:46:52 +0530

    updated as per the review comments

--- pipelineEnvVars.groovy
+++ pipelineEnvVars.groovy
@@ -9,9 +9,6 @@ def uat(){
 env.name = "reddy"
 env.age = "233"
 env.firetv = "NOK"
-env.flagship = "NOK"
-env.modules = "NOK"
-env.sharedui = "NOK"
 }
 return this
-//this is in padma branch to master
+//adding the valid comment and removing the variables
